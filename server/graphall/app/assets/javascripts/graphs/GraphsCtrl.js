aeb35f17237ecb98a7fd144c0b6278fc1c86e8b1
Chitrank Dixit <chitrankdixit@gmail.com>
2015-09-08 00:26:12 +0530

    Now created a sample view for the pie charts right now need to make view for line chart and bar chart as well in coming days

--- server/graphall/app/assets/javascripts/graphs/GraphsCtrl.js
+++ server/graphall/app/assets/javascripts/graphs/GraphsCtrl.js
@@ -8,61 +8,105 @@ function($scope){
 
     /* Chart data */
     //$scope.data = { /* JSON data */ }
-    $scope.graph_type = [
+    $scope.graph_types = [
     	{ name: 'Pie Chart', id: 3},
     	{ name: 'Line Graph',id: 1}, 
      	{ name: 'Bar Chart', id: 2}
     ];
 
-    $scope.options = {
-            chart: {
-                type: 'pieChart',
-                height: 500,
-                x: function(d){return d.key;},
-                y: function(d){return d.y;},
-                showLabels: true,
-                transitionDuration: 500,
-                labelThreshold: 0.01,
-                legend: {
-                    margin: {
-                        top: 5,
-                        right: 35,
-                        bottom: 5,
-                        left: 0
+
+    // add Name/Value dynamically code
+
+    $scope.inputs = [];
+
+    $scope.data = [];
+
+    
+
+    $scope.setGraph =  function() {
+        console.log("Graph is", $scope.graph_type);
+        if($scope.graph_type === 1)
+        {
+
+        }
+        else if($scope.graph_type === 2)
+        {
+
+        }
+        else if($scope.graph_type === 3)
+        {
+            $scope.options = {
+                chart: {
+                    type: 'pieChart',
+                    height: 500,
+                    x: function(d){return d.key;},
+                    y: function(d){return d.y;},
+                    showLabels: true,
+                    transitionDuration: 500,
+                    labelThreshold: 0.01,
+                    legend: {
+                        margin: {
+                            top: 5,
+                            right: 35,
+                            bottom: 5,
+                            left: 0
+                        }
                     }
                 }
-            }
-        };
+            };
+        }
+        else
+        {
+            console.log("Invalid Graph Type");
+        }
+    };
 
-        $scope.data = [
-            {
-                key: "One",
-                y: 5
-            },
-            {
-                key: "Two",
-                y: 2
-            },
-            {
-                key: "Three",
-                y: 9
-            },
-            {
-                key: "Four",
-                y: 7
-            },
-            {
-                key: "Five",
-                y: 4
-            },
-            {
-                key: "Six",
-                y: 3
-            },
-            {
-                key: "Seven",
-                y: .5
-            }
-        ];
+    $scope.addNameVal = function() {
+        $scope.inputs.push({});
+    };
+
+    $scope.addtoNameValQueue= function(pieslice) {
+        console.log(pieslice.name, pieslice.value);
+        var pie_slice = {
+            key: pieslice.name,
+            y: pieslice.value
+        };
+        $scope.data.push(pie_slice);
+    };
+
+
+
+    
+
+        // $scope.data = [
+        //     {
+        //         key: "One",
+        //         y: 8
+        //     },
+        //     {
+        //         key: "Two",
+        //         y: 2
+        //     },
+        //     {
+        //         key: "Three",
+        //         y: 9
+        //     },
+        //     {
+        //         key: "Four",
+        //         y: 7
+        //     },
+        //     {
+        //         key: "Five",
+        //         y: 4
+        //     },
+        //     {
+        //         key: "Six",
+        //         y: 3
+        //     },
+        //     {
+        //         key: "Seven",
+        //         y: .5
+        //     }
+        // ];
 
 }]);
